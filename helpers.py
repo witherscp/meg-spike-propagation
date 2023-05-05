3c440d14d982ef27499d833cc55e6e53b4e772bc
Kayla Togneri <kayla.togneri@nih.gov>
2023-05-05 13:53:32 -0400

    Update code

--- helpers.py
+++ helpers.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python
 
+import random
+
+import numpy as np
+import pandas as pd
+
 def retrieve_track_run(log_file, n_parcs=400):
 
-    pass
+    df = pd.read_csv(log_file, delimiter=' ', index_col='track_idx')
+    runs_with_parcs = df.loc[df.num_parc == n_parcs].index
+    
+    # ensure that only one row has correct number of parcels
+    assert runs_with_parcs.size == 1
+
+    run_num = str(runs_with_parcs[0])
+
+    if len(run_num) == 1:
+        run_num = '0' + run_num
+
+    return run_num
+
+def shuffle_nt_arr(nt_arr, n_followers, n_shuffles=10000):
+
+    nt_totals = np.full(n_shuffles, np.NaN)
+
+    for shuffle in range(n_shuffles):
+        random_source = random.sample(range(nt_arr.shape[0]), k=1)
+        possible_followers = [i for i in range(nt_arr.shape[0]) if i != random_source[0]]
+        random_followers = random.sample(possible_followers,k=n_followers)
+    
+        nt_followers = nt_arr[random_source,random_followers]
+        total_nt = np.sum(nt_followers)
+
+        nt_totals[shuffle] = total_nt
+
+    return nt_totals
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from helpers import retrieve_track_run
+from helpers import retrieve_track_run, shuffle_nt_arr
 
 neu_dir = Path("/Volumes/shares/NEU")
 dwi_dir = neu_dir / 'Projects' / 'DWI'
@@ -30,22 +30,44 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     pnum = args.pnum
-    n_parcs = args.parcs
+    n_parcs = int(args.parcs)
 
     subj_dwi_dir = dwi_dir / pnum
     subj_meg_dir = meg_dir / pnum
 
-    prob_dir = subj_dwi_dir / 'archive' / 'archive' / 'track' / 'prob'
+    prob_dir = subj_dwi_dir / 'archive' / 'track' / 'prob'
     log_path = prob_dir / 'track_log'
     run_num = retrieve_track_run(
         log_file=log_path,
         n_parcs=n_parcs
     )
 
-    all_data_path = prob_dir / run_num / 'all_data.npy'
+    all_data_path = prob_dir / run_num / 'csv' / 'all_data.npy'
     all_data = np.load(
         file=all_data_path,
         allow_pickle=True
-    ).items()
+    ).item()
+
+    nt_arr = all_data['NT']
+    nt_arr[np.diag_indices_from(nt_arr)] = 0
+
+    # TODO: loop through all available runs
+    delay_path = subj_meg_dir / 'dSPM' / 'run_07' / 'propagation_delays.csv'
+    delays = np.loadtxt(
+        fname=delay_path, 
+        delimiter=','
+    )
+
+    source_parc_idx = np.argwhere(delays == 0).squeeze()
+    follower_parc_idxs = np.argwhere(delays > 0).squeeze()
+    follower_lags = delays[delays > 0]
+
+    nt_followers = nt_arr[source_parc_idx,follower_parc_idxs]
+    total_nt = np.sum(nt_followers)
+
+    random_totals = shuffle_nt_arr(
+        nt_arr.copy(),
+        nt_followers.size
+    )
 
     print("DEBUG")
